953b739ab1b9510fcc852fe66ff91470b06842c7
graalvm-samples <wangfushun1991@gmail.com>
2025-01-26 00:06:30 +0800

    ThreadLocal

--- framework-utils/framework-util/src/main/java/com/fushun/framework/util/util/MethodTimeConsuming.java
+++ framework-utils/framework-util/src/main/java/com/fushun/framework/util/util/MethodTimeConsuming.java
@@ -1,42 +1,20 @@
 package com.fushun.framework.util.util;
 
-import cn.hutool.core.util.ObjectUtil;
-
 /**
  * 方法耗时计算
  */
 public class MethodTimeConsuming {
 
-    public static void begin() {
-        ThreadLocalUtil.setData("MethodTimeConsuming",System.currentTimeMillis());
-        Integer num=ThreadLocalUtil.getData("MethodTimeConsuming_num");
-        if (ObjectUtil.isNull(num)){
-            ThreadLocalUtil.setData("MethodTimeConsuming_num",1);
-        }else{
-            num+=1;
-            ThreadLocalUtil.setData("MethodTimeConsuming_num",num);
-        }
+    private static final ThreadLocal<Long> TIME_CONSUMING = new ThreadLocal<>();
 
+
+    public static void begin() {
+        TIME_CONSUMING.set(System.currentTimeMillis());
     }
 
     public static float consuming() {
-        Long m=ThreadLocalUtil.getData("MethodTimeConsuming");
-        if(m==null){
-            ThreadLocalUtil.removeKey("MethodTimeConsuming");
-            ThreadLocalUtil.removeKey("MethodTimeConsuming_num");
-            return 0f;
-        }
-        float consuming = (float)(System.currentTimeMillis() - m) / 1000;
-        Integer num=ThreadLocalUtil.getData("MethodTimeConsuming_num");
-        if (ObjectUtil.isNull(num)){
-            ThreadLocalUtil.removeKey("MethodTimeConsuming");
-            ThreadLocalUtil.removeKey("MethodTimeConsuming_num");
-        }
-        if(num==1){
-            ThreadLocalUtil.removeKey("MethodTimeConsuming");
-            ThreadLocalUtil.removeKey("MethodTimeConsuming_num");
-        }
-
+        float consuming = (float)(System.currentTimeMillis() - TIME_CONSUMING.get()) / 1000;
+        TIME_CONSUMING.remove();
         return consuming;
     }
 }
--- framework-utils/framework-util/src/main/java/com/fushun/framework/util/util/ThreadLocalUtil.java
+++ framework-utils/framework-util/src/main/java/com/fushun/framework/util/util/ThreadLocalUtil.java
@@ -1,73 +1,29 @@
 package com.fushun.framework.util.util;
 
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+
 
 public class ThreadLocalUtil {
 
-    static final ConcurrentHashMap<Long, ConcurrentHashMap<String, TimedValue>> map =
-            new ConcurrentHashMap<>();
+    static final ThreadLocal<ConcurrentHashMap<String,Object>> map=
+            new ThreadLocal<ConcurrentHashMap<String,Object>>();
 
-    // 定时任务的调度器
-    private static final ScheduledExecutorService cleanupExecutor = Executors.newScheduledThreadPool(1, Thread.ofVirtual().factory());
-
-    static {
-        // 安排清理任务每小时运行一次
-        cleanupExecutor.scheduleAtFixedRate(ThreadLocalUtil::cleanup, 1, 1, TimeUnit.HOURS);
+    public static <T> void setData(String key,T data){
+        get().put(key,data);
     }
 
-    public static <T> void setData(String key, T data) {
-        get().put(key, new TimedValue(data));
+    public static <T> T getData(String key){
+        return (T)get().get(key);
     }
 
-    @SuppressWarnings("unchecked")
-    public static <T> T getData(String key) {
-        TimedValue timedValue = get().get(key);
-        if (timedValue != null) {
-            timedValue.updateLastAccessTime(); // 更新上次访问时间
-            return (T) timedValue.value;
+    private static ConcurrentHashMap<String,Object> get(){
+        ConcurrentHashMap<String,Object> concurrentHashMap=map.get();
+        if(concurrentHashMap==null){
+            map.set(new ConcurrentHashMap<>());
         }
-        return null;
+        return  map.get();
     }
 
-    private static ConcurrentHashMap<String, TimedValue> get() {
-        long virtualThreadId = Thread.currentThread().threadId();
-        return map.computeIfAbsent(virtualThreadId, k -> new ConcurrentHashMap<>());
-    }
 
-    public static void remove() {
-        long virtualThreadId = Thread.currentThread().threadId();
-        map.remove(virtualThreadId);
-    }
-
-    public static void removeKey(String key) {
-        get().remove(key);
-    }
-
-    private static void cleanup() {
-        long cutoffTime = System.currentTimeMillis() - TimeUnit.HOURS.toMillis(1);
-        for (Long threadId : map.keySet()) {
-            ConcurrentHashMap<String, TimedValue> dataMap = map.get(threadId);
-            if (dataMap != null) {
-                dataMap.entrySet().removeIf(entry -> entry.getValue().lastAccessTime < cutoffTime);
-            }
-        }
-    }
-
-    // 包装类，用于存储值和时间戳
-    private static class TimedValue {
-        private final Object value;
-        private volatile long lastAccessTime;
-
-        public TimedValue(Object value) {
-            this.value = value;
-            this.lastAccessTime = System.currentTimeMillis();
-        }
-
-        public void updateLastAccessTime() {
-            this.lastAccessTime = System.currentTimeMillis();
-        }
-    }
 }
+
